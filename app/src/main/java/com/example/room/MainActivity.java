fec2b425e2d7e57d3c3661fc63b57d7dc7d4a8c3
bhagyashree12 <bhagyashreesarode15@gmail.com>
2019-12-23 09:45:39 +0530

    third commit

--- app/src/main/java/com/example/room/MainActivity.java
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.room;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Node_viewmodel viewmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewmodel = ViewModelProviders.of(this).get(Node_viewmodel.class);
+        viewmodel.getAllNotes().observe(this, new Observer<List<Node>>() {
+            @Override
+            public void onChanged(@Nullable List<Node> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/room/Node_viewmodel.java
+++ app/src/main/java/com/example/room/Node_viewmodel.java
@@ -1,6 +1,47 @@
 package com.example.room;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class Node_viewmodel extends AndroidViewModel {
+    private Node_repository repository;
+    private LiveData<List<Node>> allnotes;
+
+    public Node_viewmodel(@NonNull Application application) {
+        super(application);
+
+        repository=new Node_repository(application);
+        allnotes=repository.getNode_data();
+
+    }
+
+    public void insert(Node node){
+        repository.insert(node);
+    }
+    public void update(Node node){
+        repository.update(node);
+    }
+    public void delete(Node node){
+        repository.delete(node);
+    }
+    public void deleteall(Node node){
+        repository.delete_all();
+    }
+    public LiveData<List<Node>> getAllNotes() {
+        return allnotes;
+    }
+
+
+
+
+
+
+
+
+
 }
